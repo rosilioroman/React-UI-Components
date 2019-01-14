33ccf53dfdcc0e2d044d7c21eb76db10bbb02c13
rosilioroman <rosilioroman@gmail.com>
2019-01-14 15:12:36 -0700

    Created CardContainer and CardBanner components. Mounted CardBanner -> CardContainer -> App. Added placeholder CSS so that CardBanner is visible.

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import HeaderContainer from './components/HeaderComponents/HeaderContainer';
+import CardContainer from './components/CardComponents/CardContainer';
 import './App.css';
 
 const App = () => {
   return (
-    <div>
+    <div className="app-container">
       <h3>Welcome to React Social Card!</h3>
       <p>
         Begin by exploring the `components` directory. You'll notice we have a
@@ -19,6 +20,7 @@ const App = () => {
         </strong>
       </p>
       <HeaderContainer />
+      <CardContainer />
     </div>
   );
 };
--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,13 @@
 import React from 'react';
+import CardBanner from './CardBanner';
 import './Card.css';
+
+function CardContainer() {
+    return (
+        <div className="card-container">
+            <CardBanner />
+        </div>
+    );
+}
+
+export default CardContainer;
