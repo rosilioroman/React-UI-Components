a41c617502216115a30ddef74a900f3575cbc341
rosilioroman <rosilioroman@gmail.com>
2019-01-14 14:44:01 -0700

    Created HeaderTitle component and added it to HeaderContainer component

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+
+function HeaderTitle() {
+    return (
+        <div className="header-title">
+            <h4>Lambda School</h4>
+            <a href="https://lambdaschool.com">@LambdaSchool</a>
+            <p>26 jan</p>
+        </div>
+    )
+}
+
+export default HeaderTitle;
