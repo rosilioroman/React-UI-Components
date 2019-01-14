0ea4d0c332c9fc50eb768d4bf4ead672ad037dde
rosilioroman <rosilioroman@gmail.com>
2019-01-14 14:25:58 -0700

    Created ImageThumbnail component and added it to HeaderContainer component.

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,13 @@
 import React from 'react';
+import ImageThumbnail from './ImageThumbnail';
 import './Header.css';
+
+function HeaderContainer() {
+    return (
+        <div className="header-container">
+            <ImageThumbnail />
+        </div>
+    );
+}
+
+export default HeaderContainer;
