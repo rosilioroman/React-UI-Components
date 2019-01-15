157f9a7f24048377503bf8bb65e7918aa07b7150
rosilioroman <rosilioroman@gmail.com>
2019-01-15 15:34:19 -0700

    Created a ButtonContainer component to contain other button components. Created NumberButton component that takes text and a style through props. Created an ActionButton component that takes text and a style through props

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,23 +1,35 @@
 import React from 'react';
+import ButtonContainer from './components/ButtonComponents/ButtonContainer';
+import ActionButton from './components/ButtonComponents/ActionButton';
+import NumberButton from './components/ButtonComponents/NumberButton';
 import './App.css';
 
 const App = () => {
   return (
-    <div>
-      <h3>Welcome to React Calculator</h3>
-      <p>
-        We have given you a starter project. You'll want to build out your
-        components in their respective files, remove this code and replace it
-        with the proper components.
-      </p>
-      <p>
-        <strong>
-          Don't forget to `default export` your components and import them here
-          inside of this file in order to make them work.
-        </strong>
-      </p>
+    <div className="app-container">
+      <ButtonContainer>
+        <NumberButton bgColor="number-button" text="7"/>
+        <NumberButton bgColor="number-button" text="8"/>
+        <NumberButton bgColor="number-button" text="9"/>
+        <NumberButton bgColor="number-button" text="X"/>
+      </ButtonContainer>
     </div>
   );
 };
 
 export default App;
+
+// <div>
+    //   <h3>Welcome to React Calculator</h3>
+    //   <p>
+    //     We have given you a starter project. You'll want to build out your
+    //     components in their respective files, remove this code and replace it
+    //     with the proper components.
+    //   </p>
+    //   <p>
+    //     <strong>
+    //       Don't forget to `default export` your components and import them here
+    //       inside of this file in order to make them work.
+    //     </strong>
+    //   </p>
+    // </div>
